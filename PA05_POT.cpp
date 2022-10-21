d740917332cd6b073fd161583d8ab7e8dae626a8
Mentyyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-22 00:34:56 +0200

    Update PA05_POT.cpp

--- PA05_POT.cpp
+++ PA05_POT.cpp
@@ -1,3 +1,20 @@
+/*
+When the numbers are high enough we cannot calculate the power under 1 second.
+We don't even have to calculate that to know last digit. Example:
+2^2 = 4
+2^3 = 8
+2^4 = 16
+2^5 = 32
+2^6 = 64
+2^7 = 128
+2^8 = 256
+2^9 = 512
+Here, in the results we've got a pattern on last digits wich goes:  4, 8, 6, 2  |  4, 8, 6, 2  | ..
+And no matter if base is 2, 12, 22, or 678532 if the last digit from the base is 2 the pattern will appear.
+All we have to do is to distinguish that from 0-9 numbers.
+Base % 10 will get the last digit and the exponent % 4 will show us the number of the slot in our 2D array.
+*/
+
 #include <iostream>
 using namespace std;
 
