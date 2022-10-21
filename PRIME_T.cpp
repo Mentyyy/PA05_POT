ff20b28c0d40d5ec0050da468a1c58211adcafa9
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-22 01:39:21 +0200

    Update PRIME_T.cpp

--- PRIME_T.cpp
+++ PRIME_T.cpp
@@ -1,13 +1,26 @@
+/*
+Najważniejsze w zadaniu to zdać sobie sprawę, że wystarczy sprawdzić pierwiastek z `n`.
+Nie ma bowiem liczby, która nie ma dzielnika w przedziale 1-10, a ma go gdzieś wyżej.
+
+i * i <= n 
+to to samo co: 
+i <= sqrt( n )
+
+Potęgujemy `i` lub pierwiastkujemy `n` ( SPOJ jednak woli drugi zapis )
+
+PS: Jeżeli sqrt(n) jest liczbą rzeczywistą język C i C++ automaczynie wykona rzutowanie na typ int ucinając wszystko po przecinku.
+*/
+
 #include<iostream>
 #include<math.h>
 using namespace std;
 
-bool pierwsza(int n)
+bool pierwsza( int n )
 {
-	if (n < 2) return false; 
+	if( n < 2 ) return false; 
 
-	for (int i = 2; i <= sqrt(n); i++)
-		if (n % i == 0) return false; 
+	for( int i = 2; i <= sqrt( n ); i++ )
+		if( n % i == 0 ) return false; 
 	return true; 
 }
 
@@ -16,12 +29,12 @@ int main()
 	int C;
 	cin >> C;
 
-	for (int i = 0; i < C; i++)
+	for( int i = 0; i < C; i++ )
 	{
 		int n;
 		cin >> n;
 
-		if (pierwsza(n))
+		if( pierwsza( n ) )
 			cout << "TAK" << endl;
 		else
 			cout << "NIE" << endl;
