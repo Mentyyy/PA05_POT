0989ecfd470abe27ddce995030443e1ac1698a39
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2022-10-30 20:41:21 +0100

    Create STOS - Stos.cpp

---
+++ STOS - Stos.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int dane[10];
+int rozmiar;
+
+void push() {
+    if (rozmiar >= 10) {
+        cout << ":(" << endl;
+    }
+        else {
+            cin >> dane[rozmiar];
+            rozmiar = rozmiar + 1;
+            cout << " :) " << endl;
+        }
+}
+
+void pop() {
+    if (rozmiar >= 1) {
+        rozmiar = rozmiar - 1;
+        cout << dane[rozmiar] << endl;
+    }
+        else {
+            cout << ":(" << endl;
+        }
+}
+
+int main() {
+
+    rozmiar = 0;
+    char ch;
+
+        while (cin >> ch) {
+    
+            if (ch == '+')
+                push();
+            else if (ch == '-')
+                pop();
+        }
+    return (0);
+}
