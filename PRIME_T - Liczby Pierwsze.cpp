c75777fdcff666c8c948f0cce4f9b23cc1929a68
Mentyy <105610218+Mentyyy@users.noreply.github.com>
2023-03-31 20:33:37 +0200

    Update PRIME_T - Liczby Pierwsze.cpp

--- PRIME_T - Liczby Pierwsze.cpp
+++ PRIME_T - Liczby Pierwsze.cpp
@@ -41,3 +41,31 @@ int main()
 	}
 	return 0;
 }
+
+#include <iostream>
+
+class Log
+{
+private:
+	int m_LogLevel;
+public:
+	void SetLevel( int level )
+	{
+		m_LogLevel = level;
+	}
+
+	void Warn( const char* message )
+	{
+	
+	}
+};
+
+int main()
+{
+	Log log;
+	log.SetLevel(LogLevelWarning);
+	log.Warn("Hello!");
+	std::cin.get();
+    
+}
+
